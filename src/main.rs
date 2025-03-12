729072a838b209b075e4e00bebab9b45208bf9fd
Derek Corniello <corniedj@mail.uc.edu>
2025-03-12 11:16:58 -0400

    simple input handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use atty::Stream;
+use std::env;
+use std::io::{self, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    // Check if the user requested help
+    if args.len() > 1 && (args[1] == "--help" || args[1] == "-h") {
+        print_usage();
+        return;
+    }
+
+    if atty::is(Stream::Stdin) {
+        // No pipe input, run interactive mode
+        println!("No input detected. Running interactive mode...");
+        interactive_mode();
+    } else {
+        // Piped input, process it
+        let stdin = io::stdin();
+        let handle = stdin.lock();
+        let mut text: String = "".to_string();
+        for line in handle.lines() {
+            let line = line.expect("Failed to read line") + "\n";
+            text += &line;
+        }
+        println!("Processed input:\n{}", text);
+    }
+}
+
+fn interactive_mode() {
+    println!("This is an interactive mode. You can implement other features here.");
+}
+
+fn print_usage() {
+    println!("copycmd - simple clipboard process");
+    println!("\nUsage:");
+    println!("  copycmd");
+    println!("      Shows the current clipboard contents");
+    println!("  <command> | copycmd");
+    println!("      Pipes a stdout to your clipboard");
 }
